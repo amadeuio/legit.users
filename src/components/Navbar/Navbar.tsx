da03513c1af9500900d5c9427675da266f51ee06
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-04-29 15:24:35 +0200

    Add hands logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,16 @@
 import styles from "./Navbar.module.scss";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
+      <Image
+        width={34}
+        height={34}
+        className={styles.logo}
+        src="/logo-hands.png"
+        alt="Logo Hands"
+      />
       <h1>LEGIT.USERS</h1>
     </div>
   );
