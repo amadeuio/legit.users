e540d2a118d0b0da6f422eac930046e6d354a5dd
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-04-30 12:00:39 +0200

    Refactor UserDetails by using destructuring assignment

--- src/components/UserDetails/UserDetails.tsx
+++ src/components/UserDetails/UserDetails.tsx
@@ -6,9 +6,10 @@ import { useNavigate } from "react-router-dom";
 function UserDetails() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const numericId = parseInt(id, 10);
   const { users } = useUsersContext();
+  const numericId = parseInt(id, 10);
   const currentUser = users.find((user) => user.id === numericId);
+  const { first_name, last_name, email, createdAt, avatar } = currentUser;
 
   const handleGoBack = () => {
     navigate(-1);
@@ -17,11 +18,12 @@ function UserDetails() {
   return (
     <div className={styles.userDetails}>
       <h1>
-        {currentUser.first_name} {currentUser.last_name}
+        {first_name} {last_name}
       </h1>
-      <p>Email: {currentUser.email}</p>
+      <p>Email: {email}</p>
+      {createdAt && <p>Created at: {createdAt}</p>}
       <p onClick={handleGoBack}>
-        <img className={styles.avatar} src={currentUser.avatar} alt="User Avatar" />
+        <img className={styles.avatar} src={avatar} alt="User Avatar" />
       </p>
     </div>
   );
